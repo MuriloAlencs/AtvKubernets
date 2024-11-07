6e97847bf263ad3537133c91ee304d1eff3d8ae9
murilo <muriloalencs@gmail.com>
2024-11-07 19:44:29 -0400

    aaaa

--- start-backend.sh
+++ start-backend.sh
@@ -6,7 +6,7 @@ export FLASK_DB_TYPE="postgres"
 export FLASK_DB_USER="myuser"
 export FLASK_DB_NAME="mydb"
 export FLASK_DB_PASSWORD="mypassword"
-export FLASK_DB_HOST="localhost"
+export FLASK_DB_HOST="postgres"
 export FLASK_DB_PORT="5432"
 
 # Run app.py when the container launches
